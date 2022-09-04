1772d99d801a2b08a7c5742c0d2660940fbaaab5
nvd <navidsoleymani@ymail.com>
2022-09-04 21:06:43 +0430

    refactor

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='logging',
+    name='logger',
     version='0.01',
     license='BEENSI',
     author='beensi.com dev group',
     # author_email='email@example.com',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    url='https://github.com/beensi-packages/logging.git',
+    url='https://github.com/beensi-packages/logger.git',
     # keywords='',
     install_requires=[
         'requests',
